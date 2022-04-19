41d6c84a9e44083725af1c3eabb5b650bae200cd
PascalWo <pwolff.de@gmail.com>
2022-04-19 14:41:53 +0200

    Fetch with Input

--- src/Components/GalleryBoard.tsx
+++ src/Components/GalleryBoard.tsx
@@ -1,15 +1,26 @@
 import {Character} from "../Model/Character";
 import CharacterCard from "./CharacterCard";
 import './GalleryBoard.css'
+import {ChangeEvent, useState} from "react";
 
 type GalleryBoardProps = {
     characters : Character[]
-
 }
 
 export default function GalleryBoard({characters}: GalleryBoardProps){
-    return <div className={"gallery-board"}>{characters.map(characters => <div className={"board-character"}><CharacterCard character={characters} /></div>)}</div>
 
+    const [charactersNew, setCharactersNew] = useState<string>("")
+
+    const onFilterChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setCharactersNew(event.target.value)
+    }
+
+
+    return <div>
+        <input value={charactersNew} onChange={onFilterChange}/>
+        <button onClick={() => onFilterChange}>Click me!</button>
+        <div className={"gallery-board"}>{characters.filter(characters => characters.name.toLowerCase().includes(charactersNew.toLowerCase()) || characters.species.toLowerCase().includes(charactersNew.toLowerCase()) ).map(characters => <div className={"board-character"}><CharacterCard character={characters} /></div>)}</div>
+        </div>
 }
 
 // export default function GalleryBoard(){
