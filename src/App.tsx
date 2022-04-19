d4398568a8ee89914b52e3660daf60cebf9093c7
mstuff <mstuff@posteo.de>
2022-04-19 16:01:14 +0200

    Add useEffect and fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './App.css';
 import Title from "./Components/Title";
 import GalleryBoard from "./Components/GalleryBoard";
+import {Character} from "./Model/Character";
 
 
 const charactersApiPage1 = [
@@ -1136,13 +1137,33 @@ const charactersApiPage1 = [
 * https://c.neh.tw/thumb/f/720/comdlpng6967371.jpg*/
 
 export default function App() {
-  return (
-    <div className="App">
-      <Title />
-        <GalleryBoard charactersToDisplayOnGalleryBoard={charactersApiPage1}/>
 
-    </div>
-  );
+    const [fetchedCharactersPage1, setFetchedCharactersPage1] = useState<Character[]>([]);
+
+    const fetchCharactersApiPage1 = () =>
+        fetch("https://rickandmortyapi.com/api/character")
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    throw new Error("Network request error")
+                }
+            })
+            .catch(error => console.error(error));
+
+    useEffect(() => {
+        fetchCharactersApiPage1()
+            .then(body => setFetchedCharactersPage1(body.results))
+
+    }, []);
+
+    return (
+        <div className="App">
+            <Title/>
+            <GalleryBoard charactersToDisplayOnGalleryBoard={fetchedCharactersPage1}/>
+
+        </div>
+    );
 }
 
 
--- src/Components/GalleryBoard.tsx
+++ src/Components/GalleryBoard.tsx
@@ -19,7 +19,6 @@ export default function GalleryBoard({charactersToDisplayOnGalleryBoard}: Galler
 
     const startSearchOnClick = () => {
         setFilter(text)
-
     }
 
     return <div>
