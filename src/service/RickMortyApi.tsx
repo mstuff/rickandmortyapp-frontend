5db79a817ff2faf3363ed41ffd50fbd09e4827c1
mstuff <mstuff@posteo.de>
2022-04-20 17:57:00 +0200

    Refactor RickMortyApi

--- src/service/RickMortyApi.tsx
+++ src/service/RickMortyApi.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default function RickMortyApi (){
 
-   return    axios.get("https://rickandmortyapi.com/api/characte")
+   return    axios.get("https://rickandmortyapi.com/api/character")
             .then(response => response.data)
             .catch(error => console.error(error));
 
